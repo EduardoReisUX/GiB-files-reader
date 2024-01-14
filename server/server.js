1b9b1925c89e5edbc4a24be4c3159f7358e1d27d
Eduardo Fernandes <ducabruno09@hotmail.com>
2024-01-14 15:17:40 -0400

    feat: add web read, transform and write streams on the server

--- server/server.js
+++ server/server.js
@@ -2,14 +2,18 @@
 
 import { createServer } from "node:http";
 import { createReadStream } from "node:fs";
-import { Readable } from "node:stream";
-import { WritableStream } from "node:stream/web";
+import { Readable, Transform } from "node:stream";
+import { WritableStream, TransformStream } from "node:stream/web";
+
+import csvtojson from "csvtojson";
 
 const PORT = 3000;
 
 // curl -i -X OPTIONS -N localhost:3000
 // curl -N localhost:3000
-export const server = createServer(async (request, response) => {
+
+/** @type {import("node:http").RequestListener} */
+async function handler(request, response) {
   const headers = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "*",
@@ -22,22 +26,45 @@ export const server = createServer(async (request, response) => {
   }
 
   let items = 0;
-  Readable.toWeb(createReadStream("./animeflv.csv")).pipeTo(
-    new WritableStream({
-      /** @param {Uint8Array} chunk  */
-      write(chunk) {
-        items++;
-        response.write(chunk);
-      },
+  const readStream = createReadStream;
+  const readFileStream = readStream("./animeflv.csv");
+  const webReadStream = Readable.toWeb(readFileStream);
+  const webTransformStream = Transform.toWeb;
 
-      close() {
-        response.end();
-      },
-    })
-  );
+  webReadStream
+    .pipeThrough(webTransformStream(csvtojson()))
+    .pipeThrough(
+      new TransformStream({
+        transform(chunk, controller) {
+          const data = JSON.parse(Buffer.from(chunk).toString());
+          const mappedData = {
+            title: data.title,
+            description: data.description,
+            url_anime: data.url_anime,
+          };
 
-  response.writeHead(204, headers);
-  // response.end("ok");
-})
-  .listen(PORT)
-  .on("listening", (_) => console.log(`server is running at ${PORT}`));
+          // Quebra de linha pois é um NDJSON
+          controller.enqueue(JSON.stringify(mappedData).concat("\n"));
+        },
+      })
+    )
+    .pipeTo(
+      new WritableStream({
+        /** @param {Uint8Array} chunk  */
+        write(chunk) {
+          items++;
+          response.write(chunk);
+        },
+
+        close() {
+          response.end();
+        },
+      })
+    );
+
+  response.writeHead(200, headers);
+}
+
+export const server = createServer(handler).listen(PORT, () =>
+  console.log(`server is running at ${PORT}`)
+);
